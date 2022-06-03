252491a1c957b4670cba7a20751947f425c65434
Daniel Byrne <dbyrne978@gmail.com>
2022-06-03 16:42:42 -0400

    Update call-to-action section

--- portfolio.js
+++ portfolio.js
@@ -4,3 +4,10 @@ ResumeButtonList.forEach((button) => {
         window.open("Daniel Byrne's resume.pdf", "_blank");
     });
 });
+
+const EmailButtonList = document.querySelectorAll(".email-button");
+EmailButtonList.forEach((button) => {
+    button.addEventListener("click", () => {
+        window.open("mailto:dbyrne978@gmail.com");
+    });
+});
